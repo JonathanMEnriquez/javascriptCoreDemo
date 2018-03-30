4e1db966cf654c26fe611bc97669ec77c65fb556
user <user@users-MacBook-Pro.local>
2018-03-30 12:42:39 -0700

    added some retrieval functionality

--- JSCore/BasicsViewController.swift
+++ JSCore/BasicsViewController.swift
@@ -16,7 +16,14 @@ class BasicsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        initializeJS()
+        helloWorld()
+        firstAndLast()
+        mathTime()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +33,53 @@ class BasicsViewController: UIViewController {
     
     func initializeJS(){
         self.jsContext = JSContext()
+        
+        self.jsContext.exceptionHandler = { context, exception in
+            if let exc = exception {
+                print("JS Exception:", exc.toString())
+            }
+        }
+        
+        if let jsSourcePath = Bundle.main.path(forResource: "jssource", ofType: "js") {
+            do {
+                let jsSourceContents = try String(contentsOfFile: jsSourcePath)
+                self.jsContext.evaluateScript(jsSourceContents)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func helloWorld() {
+        if let varHelloWorld = self.jsContext.objectForKeyedSubscript("helloWorld") {
+            print(varHelloWorld.toString())
+        }
+    }
+    
+    func firstAndLast() {
+        let firstname = "Jonathan"
+        let lastname = "Enriquez"
+        
+        if let functionFullName = self.jsContext.objectForKeyedSubscript("getFullName") {
+            if let fullName = functionFullName.call(withArguments: [firstname, lastname]) {
+                print(fullName.toString())
+            }
+        }
+    }
+    
+    func mathTime() {
+        let values = [10, -5, 22, 14, -35, 101, -55, 16, 14]
+        
+        if let functionMaxMinAverage = jsContext.objectForKeyedSubscript("maxMinAverage") {
+            if let results = functionMaxMinAverage.call(withArguments: [values]) {
+                if let resultsDict = results.toDictionary() {
+                    for (k, v) in resultsDict {
+                        print(k, v)
+                    }
+                }
+            }
+        }
     }
     
     // MARK: IBAction Method
--- JSCore/jssource.js
+++ JSCore/jssource.js
@@ -1,1 +1,21 @@
-var helloWorld = "Hello World!";
+var helloWorld = "Hello World!"
+
+function getFullName(firstname, lastname) {
+    return firstname + " " + lastname;
+}
+
+function maxMinAverage(values) {
+    var max = Math.max.apply(null, values);
+    var min = Math.min.apply(null, values);
+    var sum = 0;
+    for (var i = 0; i < values.length; i++) {
+        sum += values[i];
+    }
+    var average = sum / values.length;
+    
+    return {
+        "max": max,
+        "min": min,
+        "average": average
+    };
+}
